7382e7dcae5ece8bcfca0cb895986ff59e2c3300
eduardorco <eduardorco10@gmail.com>
2024-06-19 23:17:03 -0300

    Aula 2

--- fast_api/app.py
+++ fast_api/app.py
@@ -1,8 +1,13 @@
+from http import HTTPStatus
+
+
 from fastapi import FastAPI
+from fast_api.schemas import Message
+# from fastapi.responses import HTMLResponse
 
 app = FastAPI()
 
 
-@app.get("/")
+@app.get("/",status_code=HTTPStatus.OK, response_model=Message)
 def read_root():
     return {"message": "Olá Mundo!"}
